77c99ba34a367f89691e70383d206a49d163e4c9
saivarshini <saivarshinisunkara27@gmail.com>
2025-01-10 10:35:50 +0530

    pushing thefilestorepo

--- functions.js
+++ functions.js
@@ -46,4 +46,45 @@ for (let i = 0; i < rows; i++) {
   console.log(row.trim()); 
 }
 
-  
+//! Return keyword (terminate the block stop the excection)
+function demo(a,b){
+  console.log("hi");
+  return a+b;
+};
+let c = demo(5,5);
+console.log(c);
+
+let d = (a,b) => a+b;
+let e = y(10,20);
+console.log(d);
+
+let r = (a,b)=>{return a+b};
+let q = r(10,20);
+console.log(b);
+
+//!HIGHER ORDER FUNCTION  
+// [call back is function that is argument]
+// [function which accepts another function as an argument]
+
+function hof(a){
+  return a;
+};
+
+
+//!closure 
+//it is memory where function bunded together or nested together 
+//by giving acces to inner most fun to acces the variable if outer most function}
+var v = 100;
+let j = "hi";
+function l(){
+  var user = "Varshini";
+  let company =  "amazon";
+  const sal = 1000000;
+  console.log(user);
+  console.log(company);
+  console.log(sal);
+  console.log(a , b)
+};
+l();
+
+
---
+++ programs.js
@@ -0,0 +1,11 @@
+for(let i=1;i<=5;i++){
+    for(let j=1;j<=5;j++){
+        if(i==j || i+j == 6){
+            document.wirttenln("*")
+        }else{
+            document.writeln("&nbsp;&nbsp;&nbsp;&nbsp;")
+        document.writeln("*")
+    }
+    }
+    document.write("<br>")
+}
